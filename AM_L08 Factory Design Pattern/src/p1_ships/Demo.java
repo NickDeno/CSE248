d82f5eadfc0980511e6504b9e6941d5a7ac1ab7f
Nick DeNobrega <nickdenobrega@gmail.com>
2023-05-03 13:07:50 -0400

    CSE248 Content

--- AM_L08 Factory Design Pattern/src/p1_ships/Demo.java
+++ AM_L08 Factory Design Pattern/src/p1_ships/Demo.java
@@ -3,8 +3,7 @@ package p1_ships;
 public class Demo {
     public static void main(String[] args){
         String type = "small";
-        EnemyShipFactory factory = new EnemyShipFactory();
-        EnemyShip ship1 = factory.createEnemyShip(type);
+        EnemyShip ship1 = EnemyShipFactory.createEnemyShip(type);
         System.out.println("Ship Name: " + ship1.getName() + ", Ship Speed: " + ship1.getSpeed());
     }
 }
--- AM_L08 Factory Design Pattern/src/p1_ships/EnemyShip.java
+++ AM_L08 Factory Design Pattern/src/p1_ships/EnemyShip.java
@@ -4,21 +4,20 @@ public abstract class EnemyShip {
     private String name;
     private double speed;
 
-
     public void setName(String name) {
         this.name = name;
     }
+
     public void setSpeed(double speed) {
         this.speed = speed;
     }
+
     public String getName() {
         return name;
     }
+
     public double getSpeed() {
         return speed;
     }
 
-    
-
-    
 }
